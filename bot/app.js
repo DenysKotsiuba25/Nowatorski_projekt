fe1f52e4b28bc6f7196509e792e6552049340c4e
DenysKotsiuba <denys.kotsiuba25@gmail.com>
2024-12-18 21:10:35 +0100

    Add bot/app.js

---
+++ bot/app.js
@@ -0,0 +1,15 @@
+import { Telegraf, Markup } from 'telegraf'
+import {token, webAppUrl} from './token.js'
+
+const bot = new Telegraf(token)
+
+bot.command('start', (ctx) => {
+  ctx.reply(
+    'Hello! Press to start the app',
+    Markup.inlineKeyboard([
+      Markup.button.webApp('Open mini app', `${webAppUrl}?ref=${ctx.payload}`),
+    ])
+  )
+})
+
+bot.launch()
